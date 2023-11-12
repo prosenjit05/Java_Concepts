23b8205671af727a7b64ed6c5bf6381aafd2f8fa
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-11-12 19:02:06 +0530

    constructor injection

--- ConstructorInjection/src/main/java/com/demo/ConstructorInjection/example1/Book.java
+++ ConstructorInjection/src/main/java/com/demo/ConstructorInjection/example1/Book.java
@@ -5,6 +5,11 @@ public class Book {
     private Page page;
     private Author author;
 
+    public Book(Page page, Author author) {
+        this.page = page;
+        this.author = author;
+    }
+
     public void checkValidBook(){
         // check if we have both the page and the author for a book
         if(this.page==null || this.author==null){
--- ConstructorInjection/src/main/java/com/demo/ConstructorInjection/example1/TestBook.java
+++ ConstructorInjection/src/main/java/com/demo/ConstructorInjection/example1/TestBook.java
@@ -3,7 +3,7 @@ package com.demo.ConstructorInjection.example1;
 public class TestBook {
 
     public static void main(String[] args) {
-        Book book = new Book();
+        Book book = new Book(new Page(), new Author());
 
         book.checkValidBook();
     }
