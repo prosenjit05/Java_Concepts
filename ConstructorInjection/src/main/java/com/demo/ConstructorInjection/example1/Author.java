8941d0fa21dfc9c07e841e0d1d1a8b71e44a978b
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-11-12 18:50:32 +0530

    Update Author.java

--- ConstructorInjection/src/main/java/com/demo/ConstructorInjection/example1/Author.java
+++ ConstructorInjection/src/main/java/com/demo/ConstructorInjection/example1/Author.java
@@ -6,5 +6,13 @@ public class Author {
     private String authorName;
     private String authorAddress;
 
+    public Integer getAuthorId() { return authorId; }
+    public void setAuthorId(Integer authorId) { this.authorId = authorId; }
+
+    public String getAuthorName() { return authorName; }
+    public void setAuthorName(String authorName) { this.authorName = authorName; }
+
+    public String getAuthorAddress() { return authorAddress; }
+    public void setAuthorAddress(String authorAddress) { this.authorAddress = authorAddress; }
 
 }
