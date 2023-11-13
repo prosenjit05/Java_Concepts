5b8588b31de67afb8195c80c2af62b3d8d34f27f
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-11-13 10:01:53 +0530

    modified

--- ConstructorInjection/src/main/java/com/demo/ConstructorInjection/example1/Book.java
+++ ConstructorInjection/src/main/java/com/demo/ConstructorInjection/example1/Book.java
@@ -10,21 +10,25 @@ public class Book {
     private Author author;
     private Publisher publisher;
 
-    //@Autowired
-//    public Book(Page page, Author author, Publisher publisher) {
-//        this.page = page;
-//        this.author = author;
-//        this.publisher = publisher;
-//    }
+    @Autowired
+    public Book(Page page, Author author, Publisher publisher) {
+        this.page = page;
+        this.author = author;
+        this.publisher = publisher;
+    }
 
-    @Autowired
-    public void setPage(Page page) { this.page = page; }
-    @Autowired
-    public void setAuthor(Author author) { this.author = author; }
-    @Autowired
-    public void setPublisher(Publisher publisher) { this.publisher = publisher; }
+//    @Autowired
+//    public void setPage(Page page) { this.page = page; }
+//    @Autowired
+//    public void setAuthor(Author author) { this.author = author; }
+//    @Autowired
+//    public void setPublisher(Publisher publisher) { this.publisher = publisher; }
 
     public void checkValidBook(){
+
+        //modifyPublisher();
+        this.publisher.printPublishersCount();
+
         // check if we have both the page and the author for a book
         if(this.page==null || this.author==null || this.publisher==null){
             System.out.println("Invalid Book details !!!");
@@ -33,5 +37,11 @@ public class Book {
         }
 
     }
+
+//    public void modifyPublisher(){
+//        Publisher publisher1 = new Publisher();
+//        publisher1.setMinimumNumberOfPublishers(10);
+//        this.publisher = publisher1;
+//    }
 
 }
--- ConstructorInjection/src/main/java/com/demo/ConstructorInjection/example1/Publisher.java
+++ ConstructorInjection/src/main/java/com/demo/ConstructorInjection/example1/Publisher.java
@@ -5,4 +5,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class Publisher {
 
+    private Integer minimumNumberOfPublishers = 1;
+
+    public void setMinimumNumberOfPublishers(Integer minimumNumberOfPublishers) {
+        this.minimumNumberOfPublishers = minimumNumberOfPublishers;
+    }
+
+    public void printPublishersCount(){
+        System.out.println("Minimum number of publishers are: " + minimumNumberOfPublishers);
+    }
+
 }
