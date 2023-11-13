748498d67b3f21248570fb0bc32eec1dae7973e1
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-11-13 15:16:57 +0530

    initial commit

---
+++ PropertiesYamlDemo/src/main/java/com/prop/PropertiesYamlDemo/PropertiesYamlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.prop.PropertiesYamlDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertiesYamlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertiesYamlDemoApplication.class, args);
+	}
+
+}
